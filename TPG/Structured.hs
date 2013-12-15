9782295b26744fd95486a7c7b2c8f90833d69085
sebug <sebastian.gfeller@gmail.com>
2013-12-15 10:47:02 +0100

    Now using my records

--- TPG/Structured.hs
+++ TPG/Structured.hs
@@ -10,6 +10,7 @@ import TPG.WebAPI
 import Control.Applicative
 import Control.Monad
 import Data.Aeson
+import Data.Aeson.Types
 import Data.Time.Clock
 import Data.ByteString
 import Data.ByteString.Lazy.Char8
@@ -18,19 +19,23 @@ data Connection = Connection {
   lineCode :: String,
   destinationName :: String,
   destinationCode :: String
-  }
+  } deriving (Show)
 
 data Stop = Stop {
   stopCode :: String,
   stopName :: String,
   connections :: [Connection]
-  }
+  } deriving (Show)
 
 data Stops = Stops {
   timestamp :: String,
   stops :: [Stop]
-  }
+  } deriving (Show)
 
-parseStops :: String -> Maybe Value
-parseStops = decode . Data.ByteString.Lazy.Char8.pack
+parseStops :: String -> Maybe Stops
+parseStops input = do result <- decode bsInput
+                      flip parseMaybe result $ \obj -> do
+                        ts <- obj .: "timestamp"
+                        return Stops { timestamp=ts, stops=[] }
+                   where bsInput = Data.ByteString.Lazy.Char8.pack input
 
