d49920a379bb2f4ebf33b4c75561658d64352a59
sebug <sebastian.gfeller@gmail.com>
2013-12-28 17:39:17 +0100

    Now with zero-argument call handling

--- test_tpg.hs
+++ test_tpg.hs
@@ -12,20 +12,20 @@ import TPG.Structured
 
 dispatch :: [(String, (String,[String]) -> IO())]
 dispatch = [("getStops",
-             oneArg (showResults1 . getStops) "Usage: getStops stopName"
+             oneArg (showResults1 . getStops) "getStops stopName"
             ),
             ("getNextDepartures",
-             oneArg (showResults1 . getNextDepartures) "Usage: getNextDepartures stopCode"),
+             oneArg (showResults1 . getNextDepartures) "getNextDepartures stopCode"),
             ("getAllNextDepartures",
-             threeArgs (showResults3 . getAllNextDepartures) "Usage: getAllNextDepartures stopCode lineCode destinationCode"),
+             threeArgs (showResults3 . getAllNextDepartures) "getAllNextDepartures stopCode lineCode destinationCode"),
             ("getThermometer",
-             oneArg (showResults1 . getThermometer) "Usage: getThermometer departureCode"),
+             oneArg (showResults1 . getThermometer) "getThermometer departureCode"),
             ("getThermometerPhysicalStops",
-             oneArg (showResults1 . getThermometerPhysicalStops) "Usage: getThermometerPhysicalStops departureCode"),
+             oneArg (showResults1 . getThermometerPhysicalStops) "getThermometerPhysicalStops departureCode"),
             ("getLinesColors",
-             noArg (bindShowResult . getLinesColors) "Usage: getLinesColors"),
+             noArg (bindShowResult . getLinesColors) "getLinesColors"),
             ("getDisruptions",
-             noArg (bindShowResult . getDisruptions) "Usage: getDisruptions")]
+             noArg (bindShowResult . getDisruptions) "getDisruptions")]
 
 showResults1 :: Show a => (String -> IO (Maybe a)) -> (String -> IO())
 showResults1 = (.) bindShowResult
@@ -45,31 +45,36 @@ noArg _ usage _ = putStrLn usage
 
 oneArg :: (String -> String -> IO()) -> String -> (String,[String]) -> IO()
 oneArg f _ (key,[arg1]) = f key arg1
-oneArg _ usage _ = putStrLn usage
+oneArg _ usage _ = putStrLn ("Usage: " ++ usage)
 
 twoArgs :: (String -> String -> String -> IO()) -> String -> (String,[String]) -> IO()
 twoArgs f _ (key,[arg1,arg2]) = f key arg1 arg2
-twoArgs _ usage _ = putStrLn usage
+twoArgs _ usage _ = putStrLn ("Usage: " ++ usage)
 
 threeArgs :: (String -> String -> String -> String -> IO()) -> String -> (String,[String]) -> IO()
 threeArgs f _ (key,[arg1,arg2,arg3]) = f key arg1 arg2 arg3
-threeArgs _ usage _ = putStrLn usage
+threeArgs _ usage _ = putStrLn ("Usage: " ++ usage)
 
 maybeShow :: Show a => Maybe a -> String
 maybeShow Nothing = "No result."
 maybeShow (Just o) = show o
 
 main = do
-  (command:args) <- getArgs
-  home_directory <- getHomeDirectory
-  config_handle <- openFile (home_directory ++ "/.tpg_tests") ReadMode
-  contents <- BS.hGetContents config_handle
-  let key = getApiKeyFromConfigString contents
-  case key of
-    Nothing -> error "Did not find API key"
-    Just key -> (do
-                    let mact = lookup command dispatch
-                    case mact of
-                      Nothing -> putStrLn $ "Did not find command: " ++ command
-                      Just action -> action (key,args))
-  hClose config_handle
+  args <- getArgs
+  case args of
+    [] -> do
+      putStrLn "Please provide one of the following commands as parameter:"
+      putStrLn $ foldl (++) "" $ map (\p -> (fst p) ++ "\n") dispatch
+    (command:rest) -> do
+      home_directory <- getHomeDirectory
+      config_handle <- openFile (home_directory ++ "/.tpg_tests") ReadMode
+      contents <- BS.hGetContents config_handle
+      let key = getApiKeyFromConfigString contents
+      case key of
+        Nothing -> error "Did not find API key"
+        Just key -> (do
+                        let mact = lookup command dispatch
+                        case mact of
+                          Nothing -> putStrLn $ "Did not find command: " ++ command
+                          Just action -> action (key,rest))
+      hClose config_handle
